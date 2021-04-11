a49fbaa716224384ab16302fe90c3419f04f7b5f
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-04-11 21:18:25 +0200

    Add paging by adding query from (#13)

--- src/map/Leaderboard.js
+++ src/map/Leaderboard.js
@@ -4,10 +4,11 @@ const url = require('../httpOptions')
 /**
  * Gets the leaderboard
  * @param {string} mapUid The Map UID
+ * @param {number} from Displays the top 15 from the time in milliseconds. Takes the top 1 to 15 if nothing
  * @returns {array} The list of tops
  */
-async function leaderboard(mapUid){
-    var leaderboard = await f.getData.page(url.tabs.leaderboard, 'map/' + mapUid)
+async function leaderboard(mapUid, from = 0){
+    var leaderboard = await f.getData.page(url.tabs.leaderboard, 'map/' + mapUid + '?from=' + from)
 
     leaderboard.tops.forEach(e=>{
         Object.entries(e).forEach(entry => {
