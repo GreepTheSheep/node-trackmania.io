a417f6766694d83b3a41da81fdda9c6ec363779a
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-06-01 21:46:01 +0200

    Moving rank names to a separate file

--- src/Players/players.js
+++ src/Players/players.js
@@ -63,73 +63,7 @@ class Players {
         }
 
         if (player.matchmaking.length > 0){
-            var rankNames = [
-                {
-                    "name": "Bronze 1",
-                    "abbr": "B1",
-                    "before": 300
-                },
-                {
-                    "name": "Bronze 2",
-                    "abbr": "B2",
-                    "before": 600
-                },
-                {
-                    "name": "Bronze 3",
-                    "abbr": "B3",
-                    "before": 1000
-                },
-                {
-                    "name": "Silver 1",
-                    "abbr": "S1",
-                    "before": 1300
-                },
-                {
-                    "name": "Silver 2",
-                    "abbr": "S2",
-                    "before": 1600
-                },
-                {
-                    "name": "Silver 3",
-                    "abbr": "S3",
-                    "before": 2000
-                },
-                {
-                    "name": "Gold 1",
-                    "abbr": "G1",
-                    "before": 2300
-                },
-                {
-                    "name": "Gold 2",
-                    "abbr": "G2",
-                    "before": 2600
-                },
-                {
-                    "name": "Gold 3",
-                    "abbr": "G3",
-                    "before": 3000
-                },
-                {
-                    "name": "Master 1",
-                    "abbr": "M1",
-                    "before": 3300
-                },
-                {
-                    "name": "Master 2",
-                    "abbr": "M2",
-                    "before": 3600
-                },
-                {
-                    "name": "Master 3",
-                    "abbr": "M3",
-                    "before": 4000
-                },
-                {
-                    "name": "Trackmaster",
-                    "abbr": "TM",
-                    "before": Infinity
-                }
-            ]
+            var rankNames = require('../rankNames')
             rankNames.reverse()
             player.matchmaking.find(m=>m.info.typename == '3v3').info['place'] = player.matchmaking.find(m=>m.info.typename == '3v3').info['rank']
             rankNames.forEach(rank=>{
