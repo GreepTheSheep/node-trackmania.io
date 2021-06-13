362cbfd351a9828bb666530dbc1f91ce5d9d2dfb
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-06-13 11:24:00 +0200

    Moving JSON data to a directory

---
+++ src/_appendix_datas/echelonNames.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+        name: 'No echelon',
+        echelon: 0,
+        img: "https://trackmania.io/img/echelon/0.png"
+    },
+    {
+        name: 'Bronze 1',
+        echelon: 1,
+        img: "https://trackmania.io/img/echelon/1.png"
+    },
+    {
+        name: 'Bronze 2',
+        echelon: 2,
+        img: "https://trackmania.io/img/echelon/2.png"
+    },
+    {
+        name: 'Bronze 3',
+        echelon: 3,
+        img: "https://trackmania.io/img/echelon/3.png"
+    },
+    {
+        name: 'Silver 1',
+        echelon: 4,
+        img: "https://trackmania.io/img/echelon/4.png"
+    },
+    {
+        name: 'Silver 2',
+        echelon: 5,
+        img: "https://trackmania.io/img/echelon/5.png"
+    },
+    {
+        name: 'Silver 3',
+        echelon: 6,
+        img: "https://trackmania.io/img/echelon/6.png"
+    },
+    {
+        name: 'Gold 1',
+        echelon: 7,
+        img: "https://trackmania.io/img/echelon/7.png"
+    },
+    {
+        name: 'Gold 2',
+        echelon: 8,
+        img: "https://trackmania.io/img/echelon/8.png"
+    },
+    {
+        name: 'Gold 3',
+        echelon: 9,
+        img: "https://trackmania.io/img/echelon/9.png"
+    }
+]
