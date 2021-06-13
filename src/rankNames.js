aaba0606375b4431d9e87fe78d5fac6c38ab7d90
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-06-13 11:03:49 +0200

    Add images

--- src/rankNames.js
+++ src/rankNames.js
@@ -1,80 +1,100 @@
 module.exports = {
     "3v3": [
         {
+            "name": "Starter",
+            "abbr": "S",
+            "img": "https://trackmania.io/img/mm/Starter.png",
+            "startPts": 0,
+            "endPts": 0
+        },
+        {
             "name": "Bronze 1",
             "abbr": "B1",
+            "img": "https://trackmania.io/img/mm/B1.png",
             "startPts": 0,
             "endPts": 300
         },
         {
             "name": "Bronze 2",
             "abbr": "B2",
+            "img": "https://trackmania.io/img/mm/B2.png",
             "startPts": 300,
             "endPts": 600
         },
         {
             "name": "Bronze 3",
             "abbr": "B3",
+            "img": "https://trackmania.io/img/mm/B3.png",
             "startPts": 600,
             "endPts": 1000
         },
         {
             "name": "Silver 1",
             "abbr": "S1",
+            "img": "https://trackmania.io/img/mm/S1.png",
             "startPts": 1000,
             "endPts": 1300
         },
         {
             "name": "Silver 2",
             "abbr": "S2",
+            "img": "https://trackmania.io/img/mm/S2.png",
             "startPts": 1300,
             "endPts": 1600
         },
         {
             "name": "Silver 3",
             "abbr": "S3",
+            "img": "https://trackmania.io/img/mm/S3.png",
             "startPts": 1600,
             "endPts": 2000
         },
         {
             "name": "Gold 1",
             "abbr": "G1",
+            "img": "https://trackmania.io/img/mm/G1.png",
             "startPts": 2000,
             "endPts": 2300
         },
         {
             "name": "Gold 2",
             "abbr": "G2",
+            "img": "https://trackmania.io/img/mm/G2.png",
             "startPts": 2300,
             "endPts": 2600
         },
         {
             "name": "Gold 3",
             "abbr": "G3",
+            "img": "https://trackmania.io/img/mm/G3.png",
             "startPts": 2600,
             "endPts": 3000
         },
         {
             "name": "Master 1",
             "abbr": "M1",
+            "img": "https://trackmania.io/img/mm/M1.png",
             "startPts": 3000,
             "endPts": 3300
         },
         {
             "name": "Master 2",
             "abbr": "M2",
+            "img": "https://trackmania.io/img/mm/M2.png",
             "startPts": 3300,
             "endPts": 3600
         },
         {
             "name": "Master 3",
             "abbr": "M3",
+            "img": "https://trackmania.io/img/mm/M3.png",
             "startPts": 3600,
             "endPts": 6000
         },
         {
             "name": "Trackmaster",
             "abbr": "TM",
+            "img": "https://trackmania.io/img/mm/TM.png",
             "startPts": 6000,
             "endPts": Number.MAX_VALUE
         }
@@ -83,30 +103,50 @@ module.exports = {
         {
             "name": "Unranked",
             "abbr": "U",
+            "img": {
+                "Crown": null,
+                "Lion": "https://trackmania.io/img/royal/Lion_Unranked.png"
+            },
             "startPts": 0,
             "endPts": 1
         },
         {
             "name": "Bronze",
             "abbr": "B",
+            "img": {
+                "Crown": "https://trackmania.io/img/royal/Crown_Bronze.png",
+                "Lion": "https://trackmania.io/img/royal/Lion_Bronze.png"
+            },
             "startPts": 1,
             "endPts": 10
         },
         {
             "name": "Silver",
             "abbr": "S",
+            "img": {
+                "Crown": "https://trackmania.io/img/royal/Crown_Silver.png",
+                "Lion": "https://trackmania.io/img/royal/Lion_Silver.png"
+            },
             "startPts": 10,
             "endPts": 100
         },
         {
             "name": "Gold",
             "abbr": "G",
+            "img": {
+                "Crown": "https://trackmania.io/img/royal/Crown_Gold.png",
+                "Lion": "https://trackmania.io/img/royal/Lion_Gold.png"
+            },
             "startPts": 100,
             "endPts": 1000
         },
         {
             "name": "Master",
             "abbr": "M",
+            "img": {
+                "Crown": "https://trackmania.io/img/royal/Crown_Master.png",
+                "Lion": "https://trackmania.io/img/royal/Lion_Master.png"
+            },
             "startPts": 1000,
             "endPts": Number.MAX_VALUE
         }
