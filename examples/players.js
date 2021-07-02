cad9da9beb07d9ffcdcadde0756c4888201e0459
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-07-02 23:50:02 +0200

    Fix wrong acc id (api update)

--- examples/players.js
+++ examples/players.js
@@ -20,25 +20,25 @@ function run(){
             if (p.length < 1) return console.error('nobody found')
             console.log('results', p)
 
-            players.player(p[0].accountid).then(data=>{
+            players.player(p[0].player.id).then(data=>{
                 console.log('data', data)
             }).catch(err=>{
                 console.error(err)
             })
 
-            players.playerTrophies(p[0].accountid).then(data=>{
+            players.playerTrophies(p[0].player.id).then(data=>{
                 console.log('last trophies gains', data[0])
             }).catch(err=>{
                 console.error(err)
             })
 
-            players.playerMatches(p[0].accountid, "3v3").then(data=>{
+            players.playerMatches(p[0].player.id, "3v3").then(data=>{
                 console.log('last match in 3v3 mode', data[0])
             }).catch(err=>{
                 console.error(err)
             })
 
-            players.playerMatches(p[0].accountid, "Royal").then(data=>{
+            players.playerMatches(p[0].player.id, "Royal").then(data=>{
                 console.log('last match in Royal mode', data[0])
             }).catch(err=>{
                 console.error(err)
