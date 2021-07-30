a9e33d115ba628805513c74ecaaccb63f78a4f9e
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-07-30 14:46:10 +0200

    trying to replace with a Map but still don't work

--- src/managers/PlayerManager.js
+++ src/managers/PlayerManager.js
@@ -1,17 +1,10 @@
 const Player = require('../structures/Player');
 const ReqUtil = require('../util/ReqUtil');
-const CacheManager = require('./CacheManager');
-
 class PlayerManager {
     constructor(client){
         this.client = client;
 
-        /** @private */
-        this._cache = new CacheManager();
-    }
-
-    get cache(){
-        return this._cache;
+        this.cache = new Map();
     }
 
     /**
@@ -23,7 +16,7 @@ class PlayerManager {
     async fetch(accountid, cache = this.client.options.cache){
         const player = this.client.options.api.paths.tmio.tabs.player;
         const res = await this.client._apiReq(`${new ReqUtil(this.client).tmioAPIURL}/${player}/${accountid}`);
-        if (cache) this.cache.set(res.accountid, res);
+        if (cache) this.cache = this.cache.set(res.accountid, res);
         return new Player(this.client, res);
     }
 }
