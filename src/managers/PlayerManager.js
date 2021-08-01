ca064699ce7176d4e3bf033121718f3b151a1c19
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-08-01 14:30:11 +0200

    Update get() method and fetch became private

--- src/managers/PlayerManager.js
+++ src/managers/PlayerManager.js
@@ -1,11 +1,8 @@
 const Player = require('../structures/Player');
 const ReqUtil = require('../util/ReqUtil');
 const CacheManager = require('./CacheManager');
-const EventEmitter = require('events');
-class PlayerManager extends EventEmitter {
+class PlayerManager {
     constructor(client){
-        super();
-
         this.client = client;
 
         this.cache = new CacheManager(client, Player);
@@ -14,32 +11,43 @@ class PlayerManager extends EventEmitter {
     /**
      * Fetches a player and returns its data
      * @param {String} accountid The account ID or its tm.io vanity name
-     * @param {Boolean} cache Whether to cache the player or not
+     * @param {Boolean} cache Whether to get the player from cache or not
      * @returns {Promise<Player>} The player
+     * @example
+     * // Get a player
+     * client.players.get('26d9a7de-4067-4926-9d93-2fe62cd869fc').then(player => {
+     * console.log(player.displayname);
+     * });
      */
-    async fetch(accountid, cache = this.client.options.cache.enabled){
+    async get(accountid, cache = this.client.options.cache.enabled){
+        if (cache) {
+            if (this.cache.has(accountid)) {
+                return this.cache.get(accountid);
+            } else {
+                return await this._fetch(accountid);
+            }
+        } else {
+            return await this._fetch(accountid);
+        }
+    }
+        
+    /**
+     * Fetches a player and returns its data
+     * @param {String} accountid The account ID or its tm.io vanity name
+     * @param {Boolean} cache Whether to cache the player or not
+     * @returns {Player} The player
+     * @private
+     */
+    async _fetch(accountid, cache = this.client.options.cache.enabled){
         const player = this.client.options.api.paths.tmio.tabs.player;
         const res = await this.client._apiReq(`${new ReqUtil(this.client).tmioAPIURL}/${player}/${accountid}`);
         if (cache) {
             res._cachedTimestamp = Date.now();
             this.cache.set(res.accountid, new Player(this.client, res));
-            this.emit('cacheUpdate', this.cache);
             return this.cache.get(res.accountid);
         } else {
             return new Player(this.client, res);
         }
-    }
-
-    /**
-     * Gets a player from cache
-     * @param {String} accountid The account ID or its tm.io vanity name
-     * @returns {Player} The player
-     */
-    get(accountid){
-        const res = this.cache.get(accountid);
-        if (!res) {
-            throw new Error(`Player ${accountid} not found in cache`);
-        } else return new Player(this.client, res);
     }
 }
 
