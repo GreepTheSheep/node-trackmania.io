dc4898f81e0cabab9f4d9d9d2c08323f00015a30
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-08-05 23:08:07 +0200

    Add string formatter in Utils

--- src/util/Util.js
+++ src/util/Util.js
@@ -1,5 +1,3 @@
-const has = (o, k) => Object.prototype.hasOwnProperty.call(o, k);
-
 class Util {
     /**
      * Sets default properties on an object that aren't already specified.
@@ -11,13 +9,22 @@ class Util {
     static mergeDefault(def, given) {
         if (!given) return def;
         for (const key in def) {
-            if (!has(given, key) || given[key] === undefined) {
+            if (!Object.prototype.hasOwnProperty.call(given, key) || given[key] === undefined) {
                 given[key] = def[key];
             } else if (given[key] === Object(given[key])) {
                 given[key] = Util.mergeDefault(def[key], given[key]);
             }
         }
         return given;
+    }
+
+    /**
+     * Format the string and remove the TM style code on it.
+     * @param {String} str String to format
+     * @returns {String}
+     */
+    static formatTMText(str) {
+        return str.replace(/\$[nmwoszi]|\$[hl]\[[a-zA-Z0-9/?#!&.\\\-_=@$'()+,;:]*\]|\$[0-f]{3}/gi, '');
     }
 }
 
