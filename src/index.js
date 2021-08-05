f7899d45f3b5aa0dd80e043226151239263b8ef3
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-08-05 23:08:22 +0200

    Add Util in index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 module.exports = {
 
     // Entry point for the application.
-    Client: require("./client/Client")
+    Client: require("./client/Client"),
+
+    Util: require("./util/Util"),
 };
