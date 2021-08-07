0855bb8f58520a6b271ded29824ebeefe113e152
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-08-07 21:46:57 +0200

    Add tmioURL

--- src/util/ReqUtil.js
+++ src/util/ReqUtil.js
@@ -12,6 +12,11 @@ class ReqUtil {
         return `${tmio.protocol}://${tmio.host}/${tmio.api}`;
     }
 
+    get tmioURL(){
+        const tmio = this.client.options.api.paths.tmio;
+        return `${tmio.protocol}://${tmio.host}`;
+    }
+
     get tmxAPIURL(){
         const tmx = this.client.options.api.paths.tmx;
         return `${tmx.protocol}://${tmx.host}/${tmx.api}`;
