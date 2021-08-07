c963bcb6ff57639e113e4d0aa23648c9876b102e
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-08-07 16:14:54 +0200

    Add tmxAPIURL

--- src/util/ReqUtil.js
+++ src/util/ReqUtil.js
@@ -11,6 +11,11 @@ class ReqUtil {
         const tmio = this.client.options.api.paths.tmio;
         return `${tmio.protocol}://${tmio.host}/${tmio.api}`;
     }
+
+    get tmxAPIURL(){
+        const tmx = this.client.options.api.paths.tmx;
+        return `${tmx.protocol}://${tmx.host}/${tmx.api}`;
+    }
 }
 
 module.exports = ReqUtil;
