2918274685b9b2d8410f925ec893309399785476
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2021-11-18 10:56:03 +0100

    Create rooms.test.js

---
+++ test/rooms.test.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const assert = require('assert'),
+    TMIO = require('../'),
+    tmioClient = new TMIO.Client({dev: true});
+
+describe("Rooms", function(){
+    this.timeout(15*1000);
+
+    it("Dedicated", async function(){
+        const room = await tmioClient.rooms.get(41, 769);
+
+        assert.equal(room.isCloud, false);
+        assert.equal(room.region, null);
+        assert.equal(room.login, "evoice");
+    });
+
+    it("Hosted Room", async function(){
+        const room = await tmioClient.rooms.get(15, 1476);
+
+        assert.equal(room.isCloud, true);
+        assert.equal(room.region, "eu-west");
+        assert.equal(room.login, null);
+    });
+});
