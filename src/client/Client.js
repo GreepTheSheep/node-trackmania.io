12d80c73eb8f5c4e77637f9216329987ad2ccbca
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2022-05-02 20:31:31 +0200

    Add check by monthDay if TOTD corresponds on event

--- src/client/Client.js
+++ src/client/Client.js
@@ -100,17 +100,19 @@ class Client extends BaseClient {
         setInterval(async ()=>{
             const date = DateTime.local().setZone("Europe/Paris");
             if (date.hour === 19 && date.minute === 1 && !newTotdChecked){
-                // this prevent emitting this event a second time in the same day
-                newTotdChecked = true;
-
                 let totd = await this.totd.get(date.toJSDate());
-                /**
-                 * Emitted when a new Track Of The Day is out on Trackmania.io.
-                 * <info>This event is mostly emitted one minute after the release (at 19h01 CE(S)T)</info>
-                 * @event Client#totd
-                 * @param {TOTD} totd The Track of The Day
-                 */
-                this.emit('totd', totd);
+                if (totd.monthDay === date.day) {
+                    /**
+                     * Emitted when a new Track Of The Day is out on Trackmania.io.
+                     * <info>This event is mostly emitted one minute after the release (at 19h01 CE(S)T)</info>
+                     * @event Client#totd
+                     * @param {TOTD} totd The Track of The Day
+                     */
+                    this.emit('totd', totd);
+
+                    // this prevent emitting this event a second time in the same day
+                    newTotdChecked = true;
+                }
             } else {
                 if (date.hour !== 19 && date.minute !== 1) newTotdChecked = false;
             }
